f9aa62e7b0af24d515970f76bc3ff6e53f2e0aaa
JohnyBreak <rashkost@mail.ru>
2023-12-21 00:22:14 +0700

    save type

--- Assets/Scripts/ProjectInstallers/LocalSaveInstaller.cs
+++ Assets/Scripts/ProjectInstallers/LocalSaveInstaller.cs
@@ -1,12 +1,32 @@
 using Zenject;
+using UnityEngine;
 
 public class LocalSaveInstaller : MonoInstaller
 {
+    [SerializeField] private SaveType _saveType = SaveType.Local;
+    public enum SaveType
+    {
+        Local = 1,
+        Cloud = 2,
+    }
+
     public override void InstallBindings()
     {
         IDataProvider provider;
         IPersistentData data = new PersistentData();
-        provider = new DataLocalProvider(data);
+
+        switch (_saveType)
+        {
+            case SaveType.Local:
+                provider = new DataLocalProvider(data);
+                break;
+            case SaveType.Cloud:
+                provider = new DataLocalProvider(data);//temp
+                break;
+            default:
+                provider = new DataLocalProvider(data);//temp
+                break;
+        }
 
         if (provider.TryLoad() == false) data.PlayerData = new PlayerData();
 
